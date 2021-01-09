091b99d23a87cfcede7c58f4fec0ca954e024907
Warren Simpson <warren.simpson34@gmail.com>
2021-01-09 18:58:11 -0500

    Added All Ens dataSources from their current subgraph to be used in this subgraph. The BaseRegister, EthRegisterController, and PublicResolver abis were created. The mapping now has two files storeHub and ensRegistry.

---
+++ src/ensRegistry.ts
@@ -0,0 +1,28 @@
+import { ens } from "@graphprotocol/graph-ts"
+import {
+    NewOwner,
+    NewTTL
+} from "../generated/ENSRegistry/EnsRegistry"
+import { Store, Ens } from "../generated/schema"
+
+/*
+export function handleTransfer(event: Transfer): void {
+    let node = event.params.node.toHexString();
+    let store = Store.load(event.params.owner.toHexString());
+    let ensName = Ens.load(node);
+
+    if(ensName == null && store != null) {
+        store.ensName = node;
+        ensName.store = store.id;
+    }
+    else if(ensName != null && store != null){
+
+    }
+    else if(ensName != null && store == null) {
+
+    }
+    else {
+        //Does not concern subgraph..
+    }
+  }
+  */
