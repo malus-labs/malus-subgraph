cc382cecb86318dff3a18122934728bdf56113b4
Warren Simpson <warren.simpson34@gmail.com>
2021-01-11 21:35:58 -0500

    Inside the ethRegister mapping file, the handleNameRegistered, handleNameRegisteredByController, and handleNameTransferred functions were completed. A utils file was added with functions called concat, byteArray, and uint256ByteArray.

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+import {  BigInt, ByteArray} from '@graphprotocol/graph-ts'
+
+// Helper for concatenating two byte arrays
+export  function concat(a: ByteArray, b: ByteArray): ByteArray {
+    let out = new Uint8Array(a.length + b.length)
+    for (let i = 0; i < a.length; i++) {
+      out[i] = a[i]
+    }
+    for (let j = 0; j < b.length; j++) {
+      out[a.length + j] = b[j]
+    }
+    return out as ByteArray
+}
+
+export function byteArrayFromHex(s: string): ByteArray {
+    if(s.length % 2 !== 0) {
+      throw new TypeError("Hex string must have an even number of characters")
+    }
+    let out = new Uint8Array(s.length / 2)
+    for(var i = 0; i < s.length; i += 2) {
+      out[i / 2] = parseInt(s.substring(i, i + 2), 16)
+    }
+    return out as ByteArray;
+}
+
+export function uint256ToByteArray(i: BigInt): ByteArray {
+    let hex = i.toHex().slice(2).padStart(64, '0')
+    return byteArrayFromHex(hex)
+}
