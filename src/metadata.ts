a106261545d9143093c23224e4bd8467bb28574b
Warren Simpson <warren.simpson34@gmail.com>
2021-04-27 18:02:20 -0400

    Updated the mUSDC.ts and metadata.ts. The handleMetaDataUpdated was removed from the mUSDC and added to the metadata.ts.

---
+++ src/metadata.ts
@@ -0,0 +1,23 @@
+import { MetaDataUpdated } from "../generated/FruitToken/FruitToken"
+import { Store, Domain } from "../generated/schema"
+
+export function handleMetaDataUpdated(event: MetaDataUpdated): void { 
+    let store = Store.load(event.params.store.toHexString());
+    let metaData = event.params.metaData;
+    let domain = Domain.load(metaData[0]);
+  
+    if(domain != null) {
+      if(domain.store == store.id) {
+        store.ensName = domain.id;
+        store.name = domain.name;
+        store.isVerified = domain.isVerified;
+      }
+    }
+    store.country = metaData[1];
+    store.city = metaData[2];
+    store.street = metaData[3];
+    store.website = metaData[4];
+    store.type = metaData[5];
+    store.zipcode = metaData[6];
+    store.save();
+  }
