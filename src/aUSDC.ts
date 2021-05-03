63b16437bc7845cd178b0c0da0dcbc79cf4f3626
Warren Simpson <warren.simpson34@gmail.com>
2021-05-03 11:28:56 -0400

    Made a small few updates.

--- src/aUSDC.ts
+++ src/aUSDC.ts
@@ -2,16 +2,16 @@ import { Transfer } from "../generated/aUSDC/aUSDC"
 import { Store } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
-    let toStore = Store.load(event.params.dst.toHexString());
-    let fromStore = Store.load(event.params.src.toHexString());
+    let toStore = Store.load(event.params._to.toHexString());
+    let fromStore = Store.load(event.params._from.toHexString());
 
     if(toStore != null) {
-        toStore.availableAUSDC = toStore.availableAUSDC.plus(event.params.wad);
+        toStore.availableAUSDC = toStore.availableAUSDC.plus(event.params._amount);
         toStore.save();
     }
 
     if(fromStore != null) {
-        fromStore.availableAUSDC = toStore.availableAUSDC.minus(event.params.wad);
+        fromStore.availableAUSDC = toStore.availableAUSDC.minus(event.params._amount);
         fromStore.save();
     }
 }
