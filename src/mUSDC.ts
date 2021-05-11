f653604cd1e0ed2205a23a57f86b938091658dfe
Warren Simpson <warren.simpson34@gmail.com>
2021-05-11 16:03:50 -0400

    Updated  the mUSDC.ts and subgraph.yaml.

--- src/mUSDC.ts
+++ src/mUSDC.ts
@@ -30,6 +30,11 @@ export function handleStoreCreated(event: StoreCreated): void {
   store.collateral = zeroValue;
   store.collateralRelief = zeroValue;
   store.creationDate = event.params.creationDate;
+  store.city = "";
+  store.street = "";
+  store.website = "";
+  store.type = "";
+  store.zipcode = "";
   store.save();
 }
 
