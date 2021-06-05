f69112b07dcd9e66d5f989b4f967069730ed2ddf
Warren Simpson <warren.simpson34@gmail.com>
2021-06-04 21:28:54 -0400

    Fixed bug in the USDC mapping file to prevent double addition to the availableUSDC.

--- src/USDC.ts
+++ src/USDC.ts
@@ -5,9 +5,11 @@ export function handleTransfer(event: Transfer): void {
     let toStore = Store.load(event.params.to.toHexString());
     let fromStore = Store.load(event.params.from.toHexString());
 
-    if(toStore != null) {
-        toStore.availableUSDC = toStore.availableUSDC.plus(event.params.value);
-        toStore.save();
+    if(toStore != null) { 
+        if(event.params.from.toHexString().startsWith('0x3b01442fd5b455e2e9a9a4a2ede10df6371a44ff') == false) {
+            toStore.availableUSDC = toStore.availableUSDC.plus(event.params.value);
+            toStore.save();
+        }
     }
 
     if(fromStore != null) {
