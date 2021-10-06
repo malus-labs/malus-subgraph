f1d8fc549556d1975f3ac9cb7a5a29e48cfaa4b0
Warren Simpson <warren.simpson34@gmail.com>
2021-10-06 08:37:51 -0400

    Some simple updates.

--- src/aUSDC.ts
+++ src/aUSDC.ts
@@ -2,6 +2,13 @@ import { Transfer } from "../generated/aUSDC/aUSDC"
 import { Store } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
+    /*
+    let address = byteArrayFromHex("0xbcca60bb61934080951369a648fb03df4f96263c")
+    let store_address = ethereum.Value.fromAddress(address);
+    let call = new ethereum.SmartContractCall("AToken", address, "balanceOf", "0x70a08231", [store_address])
+    let balance = ethereum.call(call);
+    */
+   
     let toStore = Store.load(event.params.to.toHexString());
     let fromStore = Store.load(event.params.from.toHexString());
 
